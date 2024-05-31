2138d8e3719b402180edbc18d0b8e6e2661a1420
Fazry Javier Nugraha <fazryjavier125@gmail.com>
2024-06-01 02:24:49 +0700

    make a content

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -87,82 +87,18 @@
                                 <div class="col-md-6">
                                     <div class="detail-box">
                                         <h1>
-                                            Digital Marketing Experts
+                                            Healthy Care
                                         </h1>
                                         <p>
-                                            Aenean scelerisque felis ut orci condimentum laoreet. Integer nisi nisl,
-                                            convallis et augue sit amet, lobortis semper quam.
+                                            Meningkatkan Kesehatan Anak-Anak Jawa Barat melalui Pemantauan Gizi,
+                                            Imunisasi, dan Analisis Data Terintegrasi
                                         </p>
-                                        <div class="btn-box">
-                                            <a href="" class="btn1">
-                                                Contact Us
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item ">
-                        <div class="container ">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="img-box">
-                                        <img src="images/slider-img.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="detail-box">
-                                        <h1>
-                                            Digital Marketing Experts
-                                        </h1>
-                                        <p>
-                                            Aenean scelerisque felis ut orci condimentum laoreet. Integer nisi nisl,
-                                            convallis et augue sit amet, lobortis semper quam.
-                                        </p>
-                                        <div class="btn-box">
-                                            <a href="" class="btn1">
-                                                Contact Us
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item ">
-                        <div class="container ">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="img-box">
-                                        <img src="images/slider-img.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="detail-box">
-                                        <h1>
-                                            Digital Marketing Experts
-                                        </h1>
-                                        <p>
-                                            Aenean scelerisque felis ut orci condimentum laoreet. Integer nisi nisl,
-                                            convallis et augue sit amet, lobortis semper quam.
-                                        </p>
-                                        <div class="btn-box">
-                                            <a href="" class="btn1">
-                                                Contact Us
-                                            </a>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <ol class="carousel-indicators">
-                    <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
-                    <li data-target="#customCarousel1" data-slide-to="1"></li>
-                    <li data-target="#customCarousel1" data-slide-to="2"></li>
-                </ol>
             </div>
 
         </section>
@@ -288,13 +224,13 @@
                             </h2>
                         </div>
                         <p>
-                            There are many variations of passages of Lorem Ipsum available, but the majority have
-                            suffered alteration
-                            in some form, by injected humour, or randomised words which don't look even slightly
-                            believable. If you
-                            are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything
-                            embarrassing hidden in
-                            the middle of text. All
+                            Kami berkomitmen meningkatkan kesehatan anak-anak di Jawa Barat melalui pemantauan
+                            gizi, imunisasi, dan penggunaan bisnis intelijen untuk analisis data kesehatan. Dengan
+                            memastikan asupan gizi yang baik dan pemantauan kesehatan yang ketat, kami berupaya mencegah
+                            masalah gizi buruk dan penyakit menular yang dapat menghambat perkembangan anak. Menggunakan
+                            data kesehatan yang terintegrasi, kami dapat mengidentifikasi area yang memerlukan perhatian
+                            lebih dan merancang program kesehatan yang lebih efektif dan tepat sasaran, demi menciptakan
+                            masyarakat yang sehat dan sejahtera di masa depan.
                         </p>
                         <a href="">
                             Read More
@@ -313,248 +249,29 @@
 
     <!-- end about section -->
 
-    <!-- case section -->
+    <!-- visualization section -->
 
     <section class="case_section layout_padding">
         <div class="container">
             <div class="heading_container">
                 <h2>
-                    Our Case Studies
+                    Visualisasi Data
                 </h2>
             </div>
-            <div class="row">
-                <div class="col-md-6">
+            <div class="row justify-content-center">
+                <div class="col-md-12 justify-content-center">
                     <div class="box">
-                        <div class="img-box">
-                            <img src="images/case-1.jpg" alt="">
-                        </div>
-                        <div class="detail-box">
-                            <h5>
-                                Sit amet consectetur adipisicing elit
-                            </h5>
-                            <p>
-                                Alteration in some form, by injected humour, or randomised words which don't look even
-                                slightly believable.
-                            </p>
-                            <a href="">
-                                <span>
-                                    Read More
-                                </span>
-                                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="box">
-                        <div class="img-box">
-                            <img src="images/case-2.jpg" alt="">
-                        </div>
-                        <div class="detail-box">
-                            <h5>
-                                Excepturi placeat nihil eos maxime
-                            </h5>
-                            <p>
-                                Alteration in some form, by injected humour, or randomised words which don't look even
-                                slightly believable.
-                            </p>
-                            <a href="">
-                                <span>
-                                    Read More
-                                </span>
-                                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-                            </a>
-                        </div>
+                        <iframe class="embed-responsive-item" width="100%" height="1800"
+                            src="https://lookerstudio.google.com/embed/reporting/ad3662ca-fe35-459c-8041-8e87bc700004/page/p_qrt8pwaphd"
+                            frameborder="0" style="border:0" allowfullscreen
+                            sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 
-    <!-- end case section -->
-
-    <!-- client section -->
-    <section class="client_section ">
-        <div class="container">
-            <div class="heading_container heading_center">
-                <h2>
-                    Testimonial
-                </h2>
-            </div>
-        </div>
-        <div id="customCarousel2" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-10 mx-auto">
-                                <div class="box">
-                                    <div class="img-box">
-                                        <img src="images/client.jpg" alt="">
-                                    </div>
-                                    <div class="detail-box">
-                                        <div class="client_info">
-                                            <div class="client_name">
-                                                <h5>
-                                                    Morojink
-                                                </h5>
-                                                <h6>
-                                                    Customer
-                                                </h6>
-                                            </div>
-                                            <i class="fa fa-quote-left" aria-hidden="true"></i>
-                                        </div>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut
-                                            labore
-                                            et
-                                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                            ullamco laboris nisi ut
-                                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                            voluptate velit esse
-                                            cillum
-                                            dolore eu fugia
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-10 mx-auto">
-                                <div class="box">
-                                    <div class="img-box">
-                                        <img src="images/client.jpg" alt="">
-                                    </div>
-                                    <div class="detail-box">
-                                        <div class="client_info">
-                                            <div class="client_name">
-                                                <h5>
-                                                    Morojink
-                                                </h5>
-                                                <h6>
-                                                    Customer
-                                                </h6>
-                                            </div>
-                                            <i class="fa fa-quote-left" aria-hidden="true"></i>
-                                        </div>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut
-                                            labore
-                                            et
-                                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                            ullamco laboris nisi ut
-                                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                            voluptate velit esse
-                                            cillum
-                                            dolore eu fugia
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-10 mx-auto">
-                                <div class="box">
-                                    <div class="img-box">
-                                        <img src="images/client.jpg" alt="">
-                                    </div>
-                                    <div class="detail-box">
-                                        <div class="client_info">
-                                            <div class="client_name">
-                                                <h5>
-                                                    Morojink
-                                                </h5>
-                                                <h6>
-                                                    Customer
-                                                </h6>
-                                            </div>
-                                            <i class="fa fa-quote-left" aria-hidden="true"></i>
-                                        </div>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut
-                                            labore
-                                            et
-                                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                            ullamco laboris nisi ut
-                                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                            voluptate velit esse
-                                            cillum
-                                            dolore eu fugia
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <ol class="carousel-indicators">
-                <li data-target="#customCarousel2" data-slide-to="0" class="active"></li>
-                <li data-target="#customCarousel2" data-slide-to="1"></li>
-                <li data-target="#customCarousel2" data-slide-to="2"></li>
-            </ol>
-        </div>
-    </section>
-    <!-- end client section -->
-
-
-
-    <!-- contact section -->
-
-    <section class="contact_section layout_padding">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-5 col-lg-4 offset-md-1">
-                    <div class="form_container">
-                        <div class="heading_container">
-                            <h2>
-                                Request A Call back
-                            </h2>
-                        </div>
-                        <form action="">
-                            <div>
-                                <input type="text" placeholder="Full Name " />
-                            </div>
-                            <div>
-                                <input type="email" placeholder="Email" />
-                            </div>
-                            <div>
-                                <input type="text" placeholder="Phone number" />
-                            </div>
-                            <div>
-                                <input type="text" class="message-box" placeholder="Message" />
-                            </div>
-                            <div class="d-flex ">
-                                <button>
-                                    SEND
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-7 px-0">
-                    <div class="map_container">
-                        <div class="map">
-                            <div id="googleMap"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- end contact section -->
+    <!-- end visualization section -->
 
     <div class="footer_container">
         <!-- info section -->
