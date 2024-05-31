5891cfb17dcfb701e42a1e27ff86dc6ea5ccfdb5
Fazry Javier Nugraha <fazryjavier125@gmail.com>
2024-05-31 23:38:04 +0700

    build the front end

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('User.index');
+});
+
+Route::get('/about', function () {
+    return view('User.about');
+});
+
+Route::get('/service', function () {
+    return view('User.service');
+});
+
+Route::get('/contact', function () {
+    return view('User.contact');
 });
